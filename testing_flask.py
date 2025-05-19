00893f899e796214fad80a55086b8b9147ac3467
Aureulius <hexegaming@gmail.com>
2025-05-20 02:47:58 +0300

    1.22

--- testing_flask.py
+++ testing_flask.py
@@ -1,13 +1,18 @@
 # autors Valdis Arelis 231RMC177
 
+# NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ NESTRĀDĀ 
+
 from flask import Flask, render_template
-from functions.functions import *
+from functions.classes import *
+
+POSTS_FILE = "txt_files/posts.txt"
 
 app = Flask(__name__)
+posts = PostList()
 
 @app.route('/')
 def index():
-    posts = load_from_file("posts.txt")
+    posts.load_from_file(POSTS_FILE)
     return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
